fb569708281e7a2787baa2154442a8635037af23
angus egbekobar <angusegbekobar@gmail.com>
2026-04-18 03:09:55 +0100

    Add AWS Terraform foundation - VPC, EC2, Security Group

---
+++ aws-terraform-foundation/variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  default = "us-east-1"
+ 
+}
+
+
+variable "project_name" {
+  default = "devops-foundation"
+
+}
+
+variable "environment" {
+  default = "production"
+
+}
+
+variable "owner" {
+  default = "Angus Egbekobar"
+}
+
+variable "ami_id" {
+  default = "ami-0c7217cdde317cfec"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
